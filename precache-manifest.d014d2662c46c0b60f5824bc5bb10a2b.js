a7da18a3f396dcee286658093ac1dea7d07dc52a
dmb-2222 <dmb-2222@yandex.ru>
2019-12-05 18:03:50 +0200

    Updates

---
+++ precache-manifest.d014d2662c46c0b60f5824bc5bb10a2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e262a83d6d33911cd9ba5c2519e1b2e9",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "3953ffff75ca5b2845c3",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.3cbcc61d.chunk.css"
+  },
+  {
+    "revision": "1ea694a5a30d1dfabb0f",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.14516b3b.chunk.js"
+  },
+  {
+    "revision": "3953ffff75ca5b2845c3",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.8f5ffb92.chunk.js"
+  },
+  {
+    "revision": "05df1f71193a6b1ae3a5",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime-main.17532924.js"
+  }
+]);
