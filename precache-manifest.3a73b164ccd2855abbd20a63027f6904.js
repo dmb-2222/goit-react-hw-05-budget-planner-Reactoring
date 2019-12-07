faed2c61fc450b99e247c1ee8c8ed19de4405fe1
dmb-2222 <dmb-2222@yandex.ru>
2019-12-07 11:43:41 +0200

    Updates

--- precache-manifest.3a73b164ccd2855abbd20a63027f6904.js
+++ precache-manifest.3a73b164ccd2855abbd20a63027f6904.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5282a3c6f10371d4d24d241f16dae33c",
+    "revision": "4a43911b788a2f68ecd3fa20c278ce5c",
     "url": "/goit-react-hw-05-budget-planner/index.html"
   },
   {
-    "revision": "039e6677fcccadcc3614",
+    "revision": "f1fd9ab6d2f1bc99df34",
     "url": "/goit-react-hw-05-budget-planner/static/css/main.3cbcc61d.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-05-budget-planner/static/js/2.a6460802.chunk.js"
   },
   {
-    "revision": "039e6677fcccadcc3614",
-    "url": "/goit-react-hw-05-budget-planner/static/js/main.c8e02b06.chunk.js"
+    "revision": "f1fd9ab6d2f1bc99df34",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.3fa3cd24.chunk.js"
   },
   {
     "revision": "05df1f71193a6b1ae3a5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-05-budget-planner/precache-manifest.549b267bcebdc76f34b60d87bbf6d449.js"
+  "/goit-react-hw-05-budget-planner/precache-manifest.3a73b164ccd2855abbd20a63027f6904.js"
 );
 
 self.addEventListener('message', (event) => {
