37cce0caff73b7f9888c1ed51cbf14a5284cb6e0
dmb-2222 <dmb-2222@yandex.ru>
2019-12-14 20:10:46 +0200

    Мемоизация запоминает предыдущее значение на один уровень

--- src/redux/selector.js
+++ src/redux/selector.js
@@ -1,12 +1,23 @@
+import { createSelector } from "reselect";
 export const getBudget = state => state.budget;
 export const getExpenseList = state => state.expenseList;
-
-export const calculateBalance = state => {
-  const budget = getBudget(state);
-  const expenses = calculateTotalExpenses(state);
-  return budget - expenses;
-};
-export const calculateTotalExpenses = state => {
-  const expenses = getExpenseList(state);
-  return expenses.reduce((total, expense) => total + expense.amount, 0);
-};
+// Мемомизация
+export const calculateTotalExpenses = createSelector(
+  [getExpenseList],
+  expenses => expenses.reduce((total, expense) => total + expense.amount, 0)
+);
+export const calculateBalance = createSelector(
+  [getBudget, calculateTotalExpenses],
+  (budget, expenses) => {
+    return (budget - expenses)
+  }
+);
+// export const calculateTotalExpenses = state => {
+//   const expenses = getExpenseList(state);
+//   return expenses.reduce((total, expense) => total + expense.amount, 0);
+// };
+// export const calculateBalance = state => {
+//   const budget = getBudget(state);
+//   const expenses = calculateTotalExpenses(state);
+//   return budget - expenses;
+// };
